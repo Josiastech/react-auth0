949b4276890f8e5338b618cfb9d4d505352f3a5a
Josias <josiastech@gmail.com>
2020-04-11 16:53:45 -0600

    Feat(core): Logout, Signup, and User Profile

--- src/App.js
+++ src/App.js
@@ -1,32 +1,41 @@
-import React, { Component } from 'react';
-import { Route } from 'react-router-dom';
-import Home from './Home';
-import Profile from './Profile';
-import Nav from './nav';
-import { render } from '@testing-library/react';
-import Auth from './Auth/Auth';
-import Callback from './Callback';
+import React, { Component } from "react";
+import { Route, Redirect } from "react-router-dom";
+import Home from "./Home";
+import Profile from "./Profile";
+import Nav from "./Nav";
+import Auth from "./Auth/Auth";
+import Callback from "./Callback";
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.auth = new Auth(this.props.history);
   }
-
   render() {
     return (
       <>
-        <Nav/>
-        <Route
-          path="/"
-          exact
-          render={props => <Home auth={this.auth} {...props} />}
-        />
-        <Route
-          path="/callback"
-          render={props => <Callback auth={this.auth} {...props} />}
-        />
-        <Route path="/profile" exact component={Profile} />
+        <Nav auth={this.auth} />
+        <div className="body">
+          <Route
+            path="/"
+            exact
+            render={props => <Home auth={this.auth} {...props} />}
+          />
+          <Route
+            path="/callback"
+            render={props => <Callback auth={this.auth} {...props} />}
+          />
+          <Route
+            path="/profile"
+            render={props =>
+              this.auth.isAuthenticated() ? (
+                <Profile auth={this.auth} {...props} />
+              ) : (
+                <Redirect to="/" />
+              )
+            }
+          />
+        </div>
       </>
     );
   }
--- src/Home.js
+++ src/Home.js
@@ -1,20 +1,20 @@
-import React, { Component } from 'react';
-import {Link} from 'react-router-dom';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 class Home extends Component {
-    render() {
-        const { isAuthenticated, login } = this.props.auth;
-        return (
-            <div>
-                <h1>Home</h1>
-                { this.props.auth.isAuthenticated() ? (
-                    <Link to="/profile">View Profile</Link>
-                ) : (
-                <button onClick={this.props.auth.login}>Log in</button>
-                )}
-            </div>
-        )
-    }
+  render() {
+    const { isAuthenticated, login } = this.props.auth;
+    return (
+      <div>
+        <h1>Home</h1>
+        {isAuthenticated() ? (
+          <Link to="/profile">View profile</Link>
+        ) : (
+          <button onClick={login}>Log In</button>
+        )}
+      </div>
+    );
+  }
 }
 
 export default Home;
