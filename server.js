1593705090752256b4da5e282a0c8781a989128f
Josias <josiastech@gmail.com>
2020-04-15 17:59:31 -0600

    API Authorization

--- server.js
+++ server.js
@@ -9,5 +9,11 @@ app.get('/public', function(req, res) {
     });
 });
 
+app.get('/private', function(req, res) {
+    res.json({
+        message: "Hello from a Private API"
+    });
+});
+
 app.listen(3001);
 console.log('API Server listenign on ' + process.env.REACT_APP_AUTH0_AUDIENCE);
